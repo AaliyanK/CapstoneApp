8fe4ffa45efcf2fb5896d19c1681f377bd33af23
Aaliyan Kapadia <aaliyan.kap@Outlook.com>
2023-02-21 17:33:20 -0500

    change about us

--- frontend/src/Components/pages/AboutUs.js
+++ frontend/src/Components/pages/AboutUs.js
@@ -9,6 +9,17 @@ const AboutUs = () => {
           <h2>About Us</h2>
           <h3>Our Mission</h3>
           <p>
+            A shift towards a circular economy has created a need to produce
+            biological products with low profit margins. To combat high capital
+            costs required for typical bioprocesses, highly accurate monitoring
+            systems are often substituted with inadequate and labour-intensive
+            procedurals. The goal of our capstone project is to develop an
+            accurate biomass monitoring system applicable for a bioprocess at a
+            fraction of the cost currently available. This will include the
+            development of a plug and play monitor combined with a user
+            interface for seamless integration.
+          </p>
+          <p>
             We aim to develop a low cost automation system for biomanufacturing
             that allows for a company to increase the productivity of their
             processes. Large amounts of labour and resources are typically spent
